d69d052e04097e05ca60ec93cd27fa60b2908c90
Onlineinfoh <tianxi730@gmail.com>
2025-02-11 21:27:41 -0500

    Updating server.py for client data prediction

--- ML/Traffic Interval/server.py
+++ ML/Traffic Interval/server.py
@@ -1,6 +1,6 @@
 # server.py 
 from flask import Flask, jsonify, request, render_template
-from lib.utils import make_predictions
+from functionalities import make_prediction
 
 app = Flask(__name__) 
 
@@ -14,8 +14,9 @@ def predictions_endpoint():
     if request.method == 'POST': 
         
         file = request.data
+
         #make_predictions change to what file is, file is data from client
-        predicted_class = make_predictions(file)
+        predicted_class = make_prediction(file)
     
     return jsonify(predicted_class.item()) 
 
@@ -26,3 +27,20 @@ if __name__ == "__main__":
     port_number = 8080 
 
     app.run(host, port_number)
+
+
+
+
+'''
+server.py is expected to send date in this format to the make_prediction function:
+
+***
+{
+    "start_date": "2023-01-01 00:00:00",
+    "prediction_length": 24,
+    "interval": "hours",
+    "junction": INTEGER
+}
+***
+
+'''
